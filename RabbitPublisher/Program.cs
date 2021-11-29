f51800533ae3d954cf52882226cd05499f6094d5
Çağlar Can SARIKAYA <63093864+caglarsarikaya@users.noreply.github.com>
2021-11-29 19:25:35 +0300

    Add project files.

---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            // factory.Uri = new Uri("amqp://guest:guest@localhost:15672");
+
+            factory.UserName = ConnectionFactory.DefaultUser;
+            factory.Password = ConnectionFactory.DefaultPass;
+            factory.VirtualHost = ConnectionFactory.DefaultVHost;
+            factory.HostName = "localhost";
+            factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+
+
+            using var conn = factory.CreateConnection();
+
+            var channel = conn.CreateModel();
+            //first is name for queue
+            //second when shut down, record on hdd or not
+            //third connectable only from here. It means you can publish here but you cant subscribe from somewhere else
+            //fourth close the queue when all connections are closed
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+
+            while(1==1)
+            {
+                string message = Console.ReadLine();
+                if (message == "0") return;
+
+                //rabbitmq accepts the messages as byte array
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                //first for selecting excehange
+                //second when didnt select an exchange, you have to send queue name
+                //third is settings
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine("Message is sent");
+            }
+             
+
+
+        }
+    }
+}
---
+++ RabbitSubscriber/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.UserName = ConnectionFactory.DefaultUser;
+            factory.Password = ConnectionFactory.DefaultPass;
+            factory.VirtualHost = ConnectionFactory.DefaultVHost;
+            factory.HostName = "localhost";
+            factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+
+
+            using var conn = factory.CreateConnection();
+
+            var channel = conn.CreateModel();
+            //creating queue is on the both side has problem, if publisher cant creates, subscribes creates. But the parameters are must be same
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            //second parameter when true rabbit will delete the message from queue without checking is revieved true
+            //real solitions has to be with false parameter we should check, is that proceed true
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Message Recieved: " + message);
+            };
+
+            Console.ReadLine();
+
+
+        }
+ 
+    }
+}
